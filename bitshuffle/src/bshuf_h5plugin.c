fb2341f35235ad1751f1ebe84a830bb332208cb9
Herbert J. Bernstein <yayahjb@gmail.com>
2020-07-12 15:06:43 -0400

    Update bshuf_h5plugin.c
    Add CBF_FILTER_STATIC conditional

--- bitshuffle/src/bshuf_h5plugin.c
+++ bitshuffle/src/bshuf_h5plugin.c
@@ -14,6 +14,7 @@
 #include "bshuf_h5filter.h"
 #include "H5PLextern.h"
 
+#ifndef CBF_FILTER_STATIC
 H5PL_type_t H5PLget_plugin_type(void) {return H5PL_TYPE_FILTER;}
 const void* H5PLget_plugin_info(void) {return bshuf_H5Filter;}
-
+#endif
